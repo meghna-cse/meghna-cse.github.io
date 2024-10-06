bc9abf1824a6916ef1d644d8dd08de8895217fc1
Meghna J <24985713+meghna-cse@users.noreply.github.com>
2024-10-05 20:38:53 -0500

    for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    eslint: {
+        ignoreDuringBuilds: true, // ESLint disabled during the build process
+    },
+};
 
 export default nextConfig;
